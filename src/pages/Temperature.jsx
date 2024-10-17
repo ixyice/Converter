4bc35eca22876cfdebcc896f66184626d6fcea6d
yayanami <115441049+Jeffrey1543@users.noreply.github.com>
2024-10-17 15:03:30 +0800

    css

--- src/pages/Temperature.jsx
+++ src/pages/Temperature.jsx
@@ -1,11 +1,52 @@
+import Form from 'react-bootstrap/Form';
+import '../styles/Temperature.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
+function SelectTemp() {
+  return (
+    <Form.Select aria-label="Default select example">
+      <option value="none" selected disabled hidden>Select</option>
+      <option>Celsius</option>
+      <option>Fahrenheit</option>
+      <option value="3">Kelvin</option>
+      <option value="3">Rankine</option>
+    </Form.Select>
+  );
+}
 
 function Temperature() {
     return(
         <> 
-            <div>
-                <input type="number" placeholder="qwe"></input>
-            </div>
+            <Container className='m-4'>
+                <Row className='p-2 border'>
+                    <h2>Temperature</h2>
+                </Row>
+                <Row className='p-4 border'>
+                    <Col> <SelectTemp /> </Col>
+                    <Col xs={2}> <h5>to</h5> </Col>
+                    <Col> <SelectTemp /> </Col>
+                </Row>
+
+                <Row className='p-4 border'>
+                    <Col>Temp 1</Col>
+                    <Col><input type="number" placeholder="qwe"></input></Col>
+                    <Col>K</Col>
+                </Row>
+
+                <Row className='p-4 border'>
+                    <Col>Temp 1</Col>
+                    <Col><input type="number" placeholder="qwe" disabled></input></Col>
+                    <Col>K</Col>
+                </Row>
+
+                
+                
+            </Container>
+
+
+            
         </>
         
     )
